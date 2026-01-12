b3988827ec8a79b840560cb1404c871f3eb1581c
paulao255 <contactpaulao@gmail.com>
2026-01-12 10:27:16 -0300

    Update ./src/Main.c in main branch.

--- src/Main.c
+++ src/Main.c
@@ -4,28 +4,29 @@
 
 /* Importations: */
 #include <stdio.h>
+#include <string.h>
 
+
+/* String Inverter version variables: */
+#define STRING_INVERTER_FULL_VERSION  111.0L /* String Inverter full version variable  (1.1.1). */
+#define STRING_INVERTER_MAJOR_VERSION   1.0L /* String Inverter major version variable     (1). */
+#define STRING_INVERTER_MINOR_VERSION   1.0L /* String Inverter minor version variable     (1). */
+#define STRING_INVERTER_PATCH_VERSION   1.0L /* String Inverter patch version variable     (1). */
 
 /* Main code: */
 int main(void)
 {
-	/* String Inverter version variables: */
-	#define STRING_INVERTER_FULL_VERSION  110.0L /* String Inverter full version variable  (1.1.0). */
-	#define STRING_INVERTER_MAJOR_VERSION   1.0L /* String Inverter major version variable     (1). */
-	#define STRING_INVERTER_MINOR_VERSION   1.0L /* String Inverter minor version variable     (1). */
-	#define STRING_INVERTER_PATCH_VERSION   0.0L /* String Inverter patch version variable     (0). */
-
 	/* Local variables: */
-	int character = 0;                           /* Stored character variable.                      */
-	char string[131072] = "";                    /* Text string variable.                           */
+	int character = 0;        /* Stored character variable. */
+	char string[131072] = ""; /* Text string variable.      */
 
 	/* Main commands: */
 	fputs("Type a text: ", stdout);
 	fgets(string, sizeof(string), stdin);
 
-	for(character = sizeof(string) - 1; character >= 0; --character)
+	for(character = (int)strlen(string) - 1; character >= 0; --character)
 	{
-		printf("%c", string[character]);
+		putchar(string[character]);
 	}
 
 	return 0;
