fa75f5d40132e7bee69ea69671e0836661c8d46b
paulao255 <contactpaulao@gmail.com>
2026-01-14 18:55:35 -0300

    Update SI-C to 2026/01/14 in main branch.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# String-Inverter-C
+# SI-C
 
 **Build/Compile process**:
+
 1. Create a directory in `.` named `./build`.
 2. Change actual directory to `./build`.
 3. Type `cmake ..` in `./build` directory to make the `./build/Makefile`.
 4. Type `make` in `./build` to run `./build/Makefile` and create the compiled file named `./build/Executable.exe`.
 
-##### Created by: @paulao255
+Created by: @paulao255.
--- src/Main.c
+++ src/Main.c
@@ -1,25 +1,24 @@
 /* Include guard: */
-#ifndef STRING_INVERTER_C
-#define STRING_INVERTER_C
+#ifndef SI_C
+#define SI_C
 
 /* Importations: */
-#include "C-Utils/cutils.h"
 #include <stdio.h>
 #include <string.h>
 
 
 /* String Inverter version variables: */
-#define STRING_INVERTER_FULL_VERSION  111.0L /* String Inverter full version variable  (1.1.1). */
-#define STRING_INVERTER_MAJOR_VERSION   1.0L /* String Inverter major version variable     (1). */
-#define STRING_INVERTER_MINOR_VERSION   1.0L /* String Inverter minor version variable     (1). */
-#define STRING_INVERTER_PATCH_VERSION   1.0L /* String Inverter patch version variable     (1). */
+#define SI_FULL_VERSION  20260114 /* String Inverter full version variable (2026/01/14). */
+#define SI_MAJOR_VERSION 2026     /* String Inverter major version variable (2026).      */
+#define SI_MINOR_VERSION 01       /* String Inverter minor version variable (01).        */
+#define SI_PATCH_VERSION 14       /* String Inverter patch version variable (14).        */
 
 /* Main code: */
 int main(void)
 {
 	/* Local variables: */
-	int character = 0;        /* Stored character variable. */
-	char string[131072] = ""; /* Text string variable.      */
+	int character = 0;        /* Stored character variable.                          */
+	char string[131072] = ""; /* Text string variable.                               */
 
 	/* Main commands: */
 	fputs("Type a text: ", stdout);
